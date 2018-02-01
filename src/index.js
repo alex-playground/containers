89ad15a3761286b6adc3fc1f6b94b29cb2d5b5b5
Alex Lubchuk <a.lubchuk@brandslisten.com>
2018-02-01 19:16:02 +0200

    add api

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import * as redux from 'redux';
 import { connect } from 'react-redux';
 import render from '@alex-playground/components';
+import '@alex-playground/api';
 
-export default function () {
+export default function (widget = '') {
   return (
     <div>
       Containers
       { render() }
+      Rendering { widget } widget.
     </div>
   );
 }
